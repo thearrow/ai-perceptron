59e8bcf0fb045cb154451d615057b56461b61775
Jake Klingler <thearrow3456@gmail.com>
2013-02-25 22:41:37 -0500

    real data and better testing

--- src/Trainer.java
+++ src/Trainer.java
@@ -34,11 +34,15 @@ public class Trainer {
     public void train(Neuron n, double learnrate) {
 
         Double err = 99999.0;
-        while (err > 0.05) {
-            for (Example e : data) {
-                err = Math.abs(n.train(e.getAttributes(), e.getTarget(), learnrate));
-                System.out.println(err);
+        int i = 0;
+        while (err > 0.0001) {
+            if (i == data.size()) {
+                i = 0;
             }
+            Example e = data.get(i);
+            err = Math.abs(n.train(e.getAttributes(), e.getTarget(), learnrate));
+            System.out.println(err);
+            i++;
         }
 
         System.out.println("Weights Learned: " + n.getWeights());
@@ -46,10 +50,19 @@ public class Trainer {
     }
 
     public void test(Neuron n) {
+        int correct = 0, total = 0;
 
         for (Example e : data) {
-            System.out.println("Expected:" + (int) Math.round(e.getTarget()) + " Actual:" + (int) Math.round(n.activate(e.getAttributes())));
+            int expected = (int) Math.round(e.getTarget());
+            int actual = (int) Math.round(n.activate(e.getAttributes()));
+            System.out.println("Expected:" + expected + " Actual:" + actual);
+            if (expected == actual) {
+                correct++;
+            }
+            total++;
         }
+
+        System.out.println("Got " + correct + "/" + total + " examples correct. (" + (double) correct / (double) total + "%)");
 
     }
 
